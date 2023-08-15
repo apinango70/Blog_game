1e182acfeb8ed2960e4f220302bc4d35aa47c9cc
Alejandro Piñango <alejandropinango@gmail.com>
2023-08-15 15:13:16 -0400

    Creación del cotrolador home/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   # root to: "home#index"
-
+  root "home#index"
 
 end
